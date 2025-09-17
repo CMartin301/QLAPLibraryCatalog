240d508c1c4fec46c51135c6f0dedb21b504279e
Clara-Martin <110358276+Clara-Martin@users.noreply.github.com>
2025-09-17 16:46:44 -0500

    Header update

---
+++ QLAPLibraryCatalogFrontend/src/components/tags/TagsPage.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const TagsPage: React.FC = () => {
+
+  
+  return (
+    <div className="container-fluid py-4 bg-pattern">
+        <div className="row mb-4">
+            <div className="col">
+                <h1 className="text-2xl md:text-3xl font-bold text-lavender-500 mb-3">
+                    Browse Tags
+                </h1>
+            </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default TagsPage;
